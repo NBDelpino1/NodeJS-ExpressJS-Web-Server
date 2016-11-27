8fd44f21bcc495c1e6a727a5e683475f5b722a7f
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-11-26 21:43:52 -0500

    fixed bug, needed CDN

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,8 @@
 
 var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Oscar",
+        "photo":"https://pbs.twimg.com/profile_images/439462178601463808/iJTi9Tw-.png",
         "scores":[
             "5",
             "1",
@@ -22,8 +22,8 @@ var friends = [
         ]
     },
     {
-        "name":"Jacob Deming",
-        "photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "name":"Grover",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/b/be/Grover.JPG",
         "scores":[
             "4",
             "2",
@@ -38,8 +38,8 @@ var friends = [
         ]
     },
     {
-        "name":"Jeremiah Scanlon",
-        "photo":"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "name":"Miss Piggy",
+        "photo":"http://images5.fanpop.com/image/photos/26900000/Miss-Piggy-miss-piggy-26998286-399-600.jpg",
         "scores":[
             "5",
             "2",
@@ -54,8 +54,8 @@ var friends = [
         ]
     },
     {
-        "name":"Louis T. Delia",
-        "photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "name":"Kermit The Frog",
+        "photo":"http://www.trendversity.com/assets/images/Kermit.jpg",
         "scores":[
             "3",
             "3",
@@ -70,8 +70,8 @@ var friends = [
         ]
     },
     {
-        "name":"Lou Ritter",
-        "photo":"https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+        "name":"Gonzo",
+        "photo":"http://img.lum.dolimg.com/v1/images/character_themuppets_gonzo_9c3596c6.jpeg?region=0,0,300,300",
         "scores":[
             "4",
             "3",
@@ -86,8 +86,8 @@ var friends = [
         ]
     },
     {
-        "name":"Jordan Biason",
-        "photo":"https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+        "name":"Animal",
+        "photo":"https://s-media-cache-ak0.pinimg.com/736x/a2/f2/0c/a2f20c4171c87f9fe3aa8bbd43443ba6.jpg",
         "scores":[
             "4",
             "4",
@@ -106,3 +106,6 @@ var friends = [
 
 // Here we export the array. This makes it accessible to other files using require.
 module.exports = friends;
+
+
+

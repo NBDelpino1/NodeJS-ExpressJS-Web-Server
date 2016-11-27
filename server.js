1533e9dba2cc4b88e274795af494c5ef5053d2c5
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-11-26 20:48:57 -0500

    set up routing, testes server and it works!

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // ==============================================================================
 // DEPENDENCIES
-// Series of npm packages that will give the server useful functionality
+// Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
 var express = require('express');
@@ -14,11 +14,11 @@ var path = require('path');
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for the express server
+// This sets up the basic properties for our express server
 // ==============================================================================
 
 var app = express(); // Tells node that we are creating an "express" server
-var PORT = process.env.PORT || 80; // Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
 
 // BodyParser makes it easy for our server to interpret data sent to it.
 // The code below is pretty standard.
@@ -35,8 +35,8 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 // ================================================================================
 // ROUTER
-// The below points the server to a series of "route" files.
-// These routes give the server a "map" of how to respond when users visits or requests data from various URLs.
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
 require('./app/routing/api-routes.js')(app);
@@ -51,7 +51,7 @@ require('./app/routing/html-routes.js')(app);
 
 // ==============================================================================
 // LISTENER
-// The below code effectively "starts" the server
+// The below code effectively "starts" our server
 // ==============================================================================
 
 app.listen(PORT, function() {
